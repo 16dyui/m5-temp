36e38947ae1c019ff6107dc6de7e1c523c633e15
5ym <16yuim@gmail.com>
2020-09-29 06:57:01 +0000

    update

--- write.php
+++ write.php
@@ -1,2 +1,7 @@
 <?php
-	file_put_contents('temp.txt', '{"data":'.file_get_contents('php://input').',"time":"'.time().'"}'."\n", FILE_APPEND);
+    $file = file('temp.txt');
+    for($i=0;60 < count($file);$i++) {
+        unset($file[$i]);
+    }
+    $file[] = '{"data":'.file_get_contents('php://input').',"time":"'.time().'"}'."\n";
+	file_put_contents('temp.txt',$file);
